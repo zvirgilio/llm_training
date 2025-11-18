1c9cfb99b52fd86af7c49633fd712f07c2c6116d
zvirgilio <zvirgilio@wustl.edu>
2025-11-17 21:28:17 -0800

    model runs fully

--- llm_model.py
+++ llm_model.py
@@ -22,6 +22,45 @@ class LLMModel(nn.Module):
 			)
 		self.lm_head = nn.Linear(config.n_embed, config.vocab_size, bias=False)
 
+		#weight sharing between encoder and head layer
+		self.transformer.wte.weight = self.lm_head.weight #orpahns wte.weight tensor and torch should clean it up
+
+		# initialize params to match gpt 2
+		self.apply(self._init_weights)
+
+	def _init_weights(self, module):
+		if isinstance(module, nn.Linear):
+			# std to 0.02 roughly matches 1/sqrt(N), the number of features incoming in gpt2 models (between 768 to 1600)
+			torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+			if module.bias is not None:
+				torch.nn.init.zeros_(module.bias)
+		elif isinstance(module, nn.Embedding):
+			torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+	def forward(self, idx, targets=None):
+		# idx is shape (B = batch dimension, T = sequence size)
+		B, T = idx.size()
+		assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is {self.config.block_size}"
+
+		#forward token and position embeddings
+		pos = torch.arange(0, T, dtype = torch.long, device=idx.device) #shape (T)
+		pos_emb = self.transformer.wpe(pos) #positions of shape (T, n_embed)
+		tok_emb = self.transformer.wte(idx) #token embeddings of shape (B, T, n_embed)
+		x = tok_emb + pos_emb
+
+		# forward through the blocks of the transformer
+		for block in self.transformer.h:
+			x = block(x)
+
+		#forward to the final layernorm and classifier
+		x = self.transformer.ln_f(x)
+		logits = self.lm_head(x) # (B, T, vocab size)
+
+		loss = None
+		if targets is not None:
+			loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+
+		return logits, loss
 
 	# use this method to load the weights from the pre-trained gpt2 from huggingface
 	@classmethod
@@ -85,13 +124,80 @@ class LLMModel(nn.Module):
 
 
 
+def get_device():
+	device = "cpu"
+	if torch.cuda.is_available():
+		device = "cuda"
+	elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+		device = "mps"
+	print(f"using device: {device}")
+	return device
+
 
 def main():
-	model = LLMModel.from_pretrained('gpt2')
-	print("didn't crash")
+	device = torch.device(get_device())
 
+	num_return_sequences = 5 
+	max_length = 30
 
+	# model = LLMModel.from_pretrained('gpt2')
+	model = LLMModel(ModelConfig())
+	
+	model.eval()
+	model.to(device)
 
+	#tokenize the prompt using tiktoken
+	import tiktoken
+	enc = tiktoken.get_encoding('gpt2')
+	with open('input.txt', 'r') as f:
+		text = f.read()
+	text = text[:1000]
+	tokens = enc.encode(text)
+	B, T = 4, 32
+	buf = torch.tensor(tokens[:B*T+1], device=device)
+	x = buf[:-1].view(B, T)
+	y = buf[1:].view(B, T)
+
+	# x.to(device)
+	# y.to(device)
+
+	logits, loss = model(x, y)
+	print(loss)
+	import sys; sys.exit(0)
+
+	# generate 
+	torch.manual_seed(42)
+	torch.mps.manual_seed(42)
+
+	while x.size(1) < max_length:
+		#forward to get logits
+		with torch.no_grad():
+			#run the model
+			logits = model(x)
+
+			#get the predictions for the next token
+			logits = logits[:, -1, :]
+
+			#compute probs
+			probs = F.softmax(logits, dim=-1)
+
+			# top-k sampling of 50 tokens (huggin face default)
+			topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+
+			# select fromt he top 50 probs
+			ix = torch.multinomial(topk_probs, 1)
+
+			# gather the corresponding indices
+			xcol = torch.gather(topk_indices, -1, ix)
+
+			#append to the sequence
+			x = torch.cat((x, xcol), dim=1)
+
+	#pring the generated text
+	for i in range(num_return_sequences):
+		tokens = x[i, :max_length].tolist()
+		decoded = enc.decode(tokens)
+		print(">", decoded)
 
 if __name__ == '__main__':
 	main()
--- llm_modules.py
+++ llm_modules.py
@@ -2,16 +2,17 @@ from dataclasses import dataclass
 import torch
 import torch.nn as nn
 from torch.nn import functional as F 
+import math 
 
 #----------------------------------------------------------------------------------------------------
 
 @dataclass 
 class ModelConfig:
-	block_size: int = 256
-	vocab_size: int = 65
-	n_layer: int = 6
-	n_head: int = 6
-	n_embed: int = 384
+	block_size: int = 1024
+	vocab_size: int = 50257
+	n_layer: int = 12
+	n_head: int = 12
+	n_embed: int = 768
 
 #----------------------------------------------------------------------------------------------------
 
